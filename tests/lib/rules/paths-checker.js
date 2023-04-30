6a8f9bab0d0346ed7bb461212c5131b6a63191e4
Oleg Skarednov <olegskar1@gmail.com>
2023-04-30 21:33:58 +0500

    добавить алиасы

--- tests/lib/rules/paths-checker.js
+++ tests/lib/rules/paths-checker.js
@@ -2,30 +2,33 @@
  * @fileoverview checking absolute paths
  * @author Oleg
  */
-"use strict";
+'use strict';
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-const rule = require("../../../lib/rules/paths-checker"),
-  RuleTester = require("eslint").RuleTester;
-
+const rule = require('../../../lib/rules/paths-checker'),
+  RuleTester = require('eslint').RuleTester;
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
-ruleTester.run("paths-checker", rule, {
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+});
+ruleTester.run('paths-checker', rule, {
   valid: [
     // give me some code that won't trigger a warning
   ],
 
   invalid: [
     {
-      code: "",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      code: "import { NotificationType } from '../../model/types/notification';",
+      errors: [
+        { message: 'В рамках одного модуля путь должен быть относительным' },
+      ],
     },
   ],
 });
